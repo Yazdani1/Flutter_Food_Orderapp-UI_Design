dea2e60c2a90edf16340d9ab22d20913d2fbe82b
Yazdani <yaz4dani@gmail.com>
2019-04-05 17:29:23 +0600

    second

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -192,12 +192,7 @@ class _HomeState extends State<Home> {
                       fontWeight: FontWeight.bold
                   ),),
 
-                new Text("Comment",
-                  style: TextStyle(
-                      fontSize: 21.0,
-                      color: Colors.grey.withOpacity(0.9),
-                      fontWeight: FontWeight.bold
-                  ),),
+               
 
 
               ],
