72004b0969907dc64ed2f48e18b8fb557755d410
Yazdani <yaz4dani@gmail.com>
2019-04-05 17:30:25 +0600

    second

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -192,10 +192,7 @@ class _HomeState extends State<Home> {
                       fontWeight: FontWeight.bold
                   ),),
 
-
-
                 
-
               ],
             ),
           )
